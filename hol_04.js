30940f0b401f202653b4c1b50d7fc62ba0373d6a
Israel <isra_quirozz@hotmail.com>
2023-04-19 19:06:19 +0200

    Complete hol-4

--- hol_04.js
+++ hol_04.js
@@ -27,7 +27,14 @@ class UserManager {
 
   addUser({ name, lastName, age, carts }) {
     let data = { name, lastName, age, carts };
-    data.id = 1;
+    let id;
+    if (!this.users.length) {
+      id = 1;
+    } else {
+      let lastUser = this.users[this.users.length - 1];
+      id = lastUser.id + 1;
+    }
+    data.id = id;
     this.users.push(data);
     let dataJson = JSON.stringify(this.users, null, 2);
     fs.promises
@@ -36,12 +43,68 @@ class UserManager {
       .catch((err) => console.log(err));
   }
 
-  // getUsers() {}
-  // getUserId(id){}
-  // updateUser(id, data){}
-  // deleteUser(id){}
+  getUsers() {
+    console.log(this.users);
+    return this.users;
+  }
+
+  getUserById(id) {
+    let foundedUser = this.users.find((user) => user.id === id);
+    if (foundedUser) {
+      console.log(foundedUser);
+      return foundedUser;
+    } else {
+      console.log(`- No se encontró un usuario con el id ${id}`);
+      return null;
+    }
+  }
+
+  updateUser(id, data) {
+    let foundedUser = this.getUserById(id);
+    if (foundedUser) {
+      for (let property in data) {
+        foundedUser[property] = data[property];
+      }
+      // BORRA EL ARCHIVO
+      fs.promises
+        .unlink(this.path)
+        .then((res) => console.log("Se elimino el archivo"))
+        .catch((err) => console.log("Ocurrió un error"));
+      // LO VOLVEMOS A CREAR CON LA INFO ACTUALIZADA
+      let dataJson = JSON.stringify(this.users, null, 2);
+      fs.promises
+        .writeFile(this.path, dataJson)
+        .then((res) => console.log("Archivo actualizado"))
+        .catch((err) => console.log(err));
+    }
+  }
+
+  deleteUser(id) {
+    let foundedUser = this.getUserById(id);
+    if (foundedUser) {
+      let newUsers = this.users.filter((user) => user.id !== id);
+      // BORRA EL ARCHIVO
+      fs.promises
+        .unlink(this.path)
+        .then((res) => console.log("Se elimino el archivo"))
+        .catch((err) => console.log("Ocurrió un error"));
+      // LO VOLVEMOS A CREAR CON LA INFO ACTUALIZADA
+      let dataJson = JSON.stringify(newUsers, null, 2);
+      fs.promises
+        .writeFile(this.path, dataJson)
+        .then((res) => console.log("Archivo actualizado"))
+        .catch((err) => console.log(err));
+      console.log(`Usuario: "${foundedUser.name}" eliminado`);
+    }
+  }
 }
 
 let manager = new UserManager("./data/users.json");
-manager.addUser({ name: "Isra", lastName: "Quiroz", age: 22, carts: [] });
-manager.addUser({ name: "Karen", lastName: "Guzmán", age: 21, carts: [] });
+// manager.addUser({ name: "Isra", lastName: "Quiroz", age: 22, carts: [] });
+// manager.addUser({ name: "Karen", lastName: "Guzmán", age: 21, carts: [] });
+// manager.addUser({ name: "Kike", lastName: "Ozuna", age: 21, carts: [] });
+
+// manager.getUsers();
+// manager.getUserById(1);
+// manager.updateUser(1, { name: "Isra" });
+manager.deleteUser(3);
